3edd110a993b1358f81a153834481583c9006031
James Noss <jnoss@stsci.edu>
2018-02-06 16:10:19 -0500

    update
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- convolution/convolve.py
+++ convolution/convolve.py
@@ -4,8 +4,10 @@
 #spec = importlib.util.spec_from_file_location("astropy", devPath+'setup_package.py')
 #astropy = importlib.util.module_from_spec(spec)
 #spec.loader.exec_module(astropy)
+import sys
+sys.path.insert(0, '/Users/jnoss/dev/astropy/build/lib.macosx-10.6-x86_64-3.5/')
 import astropy
-print(astropy.version.version)
+print(astropy.version)
 
 import astropy.convolution as astroconv
 import scipy.ndimage.filters as sciconv
@@ -16,7 +18,7 @@ import gc
 import time
 #import gputools
 
-iLength = 100#00
+iLength = 10000
 jLength = iLength
 image = np.random.random((iLength, jLength))
 
@@ -26,9 +28,9 @@ ker = astroconv.Gaussian2DKernel(20,x_size=size,y_size=size)
 
 #smoothed = scisig.fftconvolve(image, ker)#.array, mode='same')
 #smoothed = astroconv.convolve_fft(image, ker, allow_huge=True)#, boundary='wrap')
-
+#image[50, 50] = np.nan
 #smoothed = sciconv.convolve(image, ker.array, mode='reflect')
-astroconv.convolve(image, ker, boundary=None)
+astroconv.convolve(image, ker, boundary=None, nan_treatment='interpolate')
 
 
 #smoothed = sciconv.convolve(image, ker)
@@ -39,5 +41,5 @@ astroconv.convolve(image, ker, boundary=None)
 
 #quote, rem = scisig.deconvolve(image, ker)
 
-gc.collect()
-time.sleep(60)
+#gc.collect()
+#time.sleep(60)
